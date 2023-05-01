584fe581a9b23fc2afb67667c2d518c902c65c67
max <kovalev.ma@ukr.net>
2023-05-01 18:21:11 +0300

    homework_7

--- homework_7.py
+++ homework_7.py
@@ -1,15 +1,16 @@
-
 triangle_width = int(input('Введить ширину трикутника '))
 triangle_width += 1
+symbol_ = '*'
+space_ = ' '
 print('Трикутник №1')
 for i in range(1, triangle_width):
-    print('*' * (triangle_width - i))
+    print(symbol_ * (triangle_width - i))
 print('Трикутник №2')
 for i in range(1, triangle_width):
-    print('*' * i)
+    print(symbol_ * i)
 print('Трикутник №3')
 for i in range(1, triangle_width):
-    print(' ' * (i - 1) + '*' * (triangle_width - i))
+    print(space_ * (i - 1) + symbol_ * (triangle_width - i))
 print('Трикутник №4')
 for i in range(1, triangle_width):
-    print(' ' * (triangle_width - i - 1) + ('*' * i))
+    print(space_ * (triangle_width - i - 1) + (symbol_ * i))
--- homework_8.py
+++ homework_8.py
@@ -1,3 +1,22 @@
-
-print('Homework_8')
-input()
+program_repeat = 'y'
+while program_repeat == 'y':
+    print('Програма знаходить усі числа кратні 3 в діапазоні від 1 до N де N - ціле число')
+    n = int(input('Введить число N '))
+    multiple_of = 3
+    while n > 1:
+        if n % multiple_of == 0:
+            print(n, end=' ')
+        n -= 1
+    print('\nПрограма знаходить сумму усіх чисел кратніх 3 в діапазоні від 1 до N де N - ціле число')
+    n = int(input('Введить число N '))
+    summ_multiple_of = 0
+    while n > 1:
+        if n % multiple_of == 0:
+            if n == multiple_of:
+                print(n, end=' = ')
+            else:
+                print(n, end=' + ')
+        summ_multiple_of += n
+        n -= 1
+    print(summ_multiple_of)
+    program_repeat = input('Бажаєте перезапустити програму? y/n ')
